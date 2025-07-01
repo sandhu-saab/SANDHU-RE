92d30006ce7008659a105e511453920d4e3cbde9
sandhu-saab <sandhusaab61291@gmail.com>
2025-07-01 16:21:48 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,190 @@
+import re, os, time
+id_pattern = re.compile(r'^.\d+$') 
+
+class Config(object):
+    # digital_botz client config
+    API_ID = os.environ.get("API_ID", "29882686")
+    API_HASH = os.environ.get("API_HASH", "b642a25aee67b2aed02116df4a916bca")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+
+    # premium account string session required 😢 
+    STRING_SESSION = os.environ.get("STRING_SESSION", "")
+    
+    # database config
+    DB_NAME = os.environ.get("DB_NAME","sukhwindersandhu00445")     
+    DB_URL = os.environ.get("DB_URL","mongodb+srv://sukhwindersandhu00445:sandhu80544@cluster0.bn3iigb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+ 
+    # other configs
+    RKN_PIC = os.environ.get("RKN_PIC", "https://i.ibb.co/R4B4RHDr/photo-2025-06-30-00-36-57-7521534951401979912.jpg")
+    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6316008361 6157414954 6796307271').split()]
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002122152204"))
+
+    # free upload limit 
+    FREE_UPLOAD_LIMIT = 6442450944 # calculation 6*1024*1024*1024=results
+
+    # premium mode feature ✅
+    UPLOAD_LIMIT_MODE = True 
+    PREMIUM_MODE = True 
+    
+    #force subs
+    try:
+        FORCE_SUB = int(os.environ.get("FORCE_SUB", "")) 
+    except:
+        FORCE_SUB = os.environ.get("FORCE_SUB", "OttSandhu")
+        
+    # wes response configuration     
+    PORT = int(os.environ.get("PORT", "8080"))
+    BOT_UPTIME = time.time()
+
+class rkn(object):
+    # part of text configuration
+    START_TXT = """<b>Ｈ𝙰𝙸, {}👋
+
+𝚃ʜɪs 𝙸s 𝙰ɴ 𝙰ᴅᴠᴀᴄᴇᴅ 𝙰ɴᴅ 𝚈ᴇᴛ 𝙿ᴏᴡᴇʀғᴜʟ 𝚁ᴇɴᴀᴍᴇ 𝙱ᴏᴛ
+𝚄sɪɴɢ 𝚃ʜɪs 𝙱ᴏᴛ 𝚈ᴏᴜ 𝙲ᴀɴ 𝚁ᴇɴᴀᴍᴇ & 𝙲ʜᴀɴɢᴇ 𝚃ʜᴜᴍʙɴᴀɪʟ 𝙾ғ 𝚈ᴏᴜʀ 𝙵ɪʟᴇ 
+𝚈ᴏᴜ 𝙲ᴀɴ 𝙰ʟsᴏ 𝙲ᴏɴᴠᴇʀᴛ 𝚅ɪᴅᴇᴏ 𝚃ᴏ 𝙵ɪʟᴇ & 𝙵ɪʟᴇ 𝚃ᴏ 𝚅ɪᴅᴇᴏ
+𝚃𝙷𝙸𝚂 𝙱𝙾𝚃 𝙰𝙻𝚂𝙾 𝚂𝚄𝙿𝙿𝙾𝚁𝚃𝚂 𝙲𝚄𝚂𝚃𝙾𝙼 𝚃𝙷𝚄𝙼𝙱𝙽𝙰𝙸𝙻 𝙰𝙽𝙳 𝙲𝚄𝚂𝚃𝙾𝙼 𝙲𝙰𝙿𝚃𝙸𝙾𝙽
+
+Tʜɪs Bᴏᴛ Wᴀs Cʀᴇᴀᴛᴇᴅ Bʏ : @OttSandhu 💞</b>"""
+
+    ABOUT_TXT = """<b>╭───────────⍟
+├🤖 ᴍy ɴᴀᴍᴇ : {}
+├🖥️ Dᴇᴠᴇʟᴏᴩᴇʀꜱ : {}
+├👨‍💻 Pʀᴏɢʀᴀᴍᴇʀ : {}
+├📕 Lɪʙʀᴀʀy : {}
+├✏️ Lᴀɴɢᴜᴀɢᴇ: {}
+├💾 Dᴀᴛᴀ Bᴀꜱᴇ: {}
+├📊 ᴠᴇʀsɪᴏɴ: <a href=https://t.me/OttSandhu>{}</a></b>     
+╰───────────────⍟ """
+
+    HELP_TXT = """
+<b>•></b> /start Tʜᴇ Bᴏᴛ.
+
+✏️ <b><u>Hᴏᴡ Tᴏ Rᴇɴᴀᴍᴇ A Fɪʟᴇ</u></b>
+<b>•></b> Sᴇɴᴅ Aɴy Fɪʟᴇ Aɴᴅ Tyᴩᴇ Nᴇᴡ Fɪʟᴇ Nɴᴀᴍᴇ \nAɴᴅ Aᴇʟᴇᴄᴛ Tʜᴇ Fᴏʀᴍᴀᴛ [ document, video, audio ].           
+ℹ️ 𝗔𝗻𝘆 𝗢𝘁𝗵𝗲𝗿 𝗛𝗲𝗹𝗽 𝗖𝗼𝗻𝘁𝗮𝗰𝘁 :- <a href=https://t.me/Baii_Ji>Baii_Ji</a>
+"""
+
+    UPGRADE_PREMIUM= """
+•⪼ ★𝘗𝘭𝘢𝘯𝘴    -  ⏳𝘋𝘢𝘵𝘦 - 💸𝘗𝘳𝘪𝘤𝘦 
+•⪼ 🥉𝘉𝘳𝘰𝘯𝘻𝘦  -   3𝘥𝘢𝘺𝘴 -   39
+•⪼ 🥈𝘚𝘪𝘭𝘷𝘦𝘳   -   7𝘥𝘢𝘺𝘴 -   59
+•⪼ 🥇𝘎𝘰𝘭𝘥    -  15𝘥𝘢𝘺𝘴 -  99
+•⪼ 🏆𝘗𝘭𝘢𝘵𝘪𝘯𝘶𝘮 -  1𝘮𝘰𝘯𝘵𝘩 -  179
+•⪼ 💎𝘋𝘪𝘢𝘮𝘰𝘯𝘥 -  2𝘮𝘰𝘯𝘵𝘩 -  339
+
+- 𝘋𝘢𝘪𝘭𝘺 𝘜𝘱𝘭𝘰𝘢𝘥 𝘓𝘪𝘮𝘪𝘵 𝘜𝘯𝘭𝘪𝘮𝘪𝘵𝘦𝘥
+- 𝘋𝘪𝘴𝘤𝘰𝘶𝘯𝘵 𝘈𝘭𝘭 𝘗𝘭𝘢𝘯 𝘙𝘴.9
+    """
+    
+    UPGRADE_PLAN= """
+𝘗𝘭𝘢𝘯: 𝘗𝘳𝘰
+𝘋𝘢𝘵𝘦: 1 𝘮𝘰𝘯𝘵𝘩 
+𝘗𝘳𝘪𝘤𝘦: 179
+𝘓𝘪𝘮𝘪𝘵: 100 𝘎𝘉
+
+𝘗𝘭𝘢𝘯: 𝘜𝘭𝘵𝘢 𝘗𝘳𝘰 
+𝘋𝘢𝘵𝘦: 1 𝘮𝘰𝘯𝘵𝘩 
+𝘗𝘳𝘪𝘤𝘦: 199
+𝘓𝘪𝘮𝘪𝘵: 1000 𝘎𝘉
+
+- 𝘋𝘪𝘴𝘤𝘰𝘶𝘯𝘵 𝘈𝘭𝘭 𝘗𝘭𝘢𝘯 𝘙𝘴.9
+    """
+    
+    THUMBNAIL = """
+🌌 <b><u>Hᴏᴡ Tᴏ Sᴇᴛ Tʜᴜᴍʙɴɪʟᴇ</u></b>
+
+<b>•></b> Sᴇɴᴅ Aɴy Pʜᴏᴛᴏ Tᴏ Aᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟy Sᴇᴛ Tʜᴜᴍʙɴɪʟᴇ.
+<b>•></b> /del_thumb Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Dᴇʟᴇᴛᴇ Yᴏᴜʀ Oʟᴅ Tʜᴜᴍʙɴɪʟᴇ.
+<b>•></b> /view_thumb Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Vɪᴇᴡ Yᴏᴜʀ Cᴜʀʀᴇɴᴛ Tʜᴜᴍʙɴɪʟᴇ.
+"""
+    CAPTION= """
+📑 <b><u>Hᴏᴡ Tᴏ Sᴇᴛ Cᴜꜱᴛᴏᴍ Cᴀᴩᴛɪᴏɴ</u></b>
+
+<b>•></b> /set_caption - Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Sᴇᴛ ᴀ Cᴜꜱᴛᴏᴍ Cᴀᴩᴛɪᴏɴ
+<b>•></b> /see_caption - Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Vɪᴇᴡ Yᴏᴜʀ Cᴜꜱᴛᴏᴍ Cᴀᴩᴛɪᴏɴ
+<b>•></b> /del_caption - Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Dᴇʟᴇᴛᴇ Yᴏᴜʀ Cᴜꜱᴛᴏᴍ Cᴀᴩᴛɪᴏɴ
+
+Exᴀᴍᴩʟᴇ:- `/set_caption 📕 Fɪʟᴇ Nᴀᴍᴇ: {filename}
+💾 Sɪᴢᴇ: {filesize}
+⏰ Dᴜʀᴀᴛɪᴏɴ: {duration}`
+"""
+    BOT_STATUS = """
+⚡️ ʙᴏᴛ sᴛᴀᴛᴜs ⚡️
+
+⌚️ ʙᴏᴛ ᴜᴩᴛɪᴍᴇ: `{}`
+👭 ᴛᴏᴛᴀʟ ᴜsᴇʀꜱ: `{}`
+💸 ᴛᴏᴛᴀʟ ᴘʀᴇᴍɪᴜᴍ ᴜsᴇʀs: `{}`
+֍ ᴜᴘʟᴏᴀᴅ: `{}`
+⊙ ᴅᴏᴡɴʟᴏᴀᴅ: `{}`
+"""
+    LIVE_STATUS = """
+⚡ ʟɪᴠᴇ sᴇʀᴠᴇʀ sᴛᴀᴛᴜs ⚡
+
+ᴜᴘᴛɪᴍᴇ: `{}`
+ᴄᴘᴜ: `{}%`
+ʀᴀᴍ: `{}%` 
+ᴛᴏᴛᴀʟ ᴅɪsᴋ: `{}`
+ᴜsᴇᴅ sᴘᴀᴄᴇ: `{} {}%`
+ғʀᴇᴇ sᴘᴀᴄᴇ: `{}`
+ᴜᴘʟᴏᴀᴅ: `{}`
+ᴅᴏᴡɴʟᴏᴀᴅ: `{}`
+V𝟹.𝟶.𝟶 [STABLE]
+"""
+    DIGITAL_METADATA = """
+❪ SET CUSTOM METADATA ❫
+
+- /metadata - Tᴏ Sᴇᴛ & Cʜᴀɴɢᴇ ʏᴏᴜʀ ᴍᴇᴛᴀᴅᴀᴛᴀ ᴄᴏᴅᴇ
+
+☞ Fᴏʀ Exᴀᴍᴘʟᴇ:-
+
+`--change-title TG - OttSandhu
+--change-video-title [OttSandhu]
+--change-audio-title [OttSandhu]
+--change-subtitle-title [OttSandhu]
+--change-author [OttSandhu]`
+
+📥 Fᴏʀ Hᴇʟᴘ Cᴏɴᴛ. @Digital_Botz
+"""
+    
+    CUSTOM_FILE_NAME = """
+<u>🖋️ Custom File Name</u>
+
+you can pre-add a prefix and suffix along with your new filename
+
+➢ /set_prefix - To add a prefix along with your _filename.
+➢ /see_prefix - Tᴏ Sᴇᴇ Yᴏᴜʀ Pʀᴇғɪx !!
+➢ /del_prefix - Tᴏ Dᴇʟᴇᴛᴇ Yᴏᴜʀ Pʀᴇғɪx !!
+➢ /set_suffix - To add a suffix along with your filename_.
+➢ /see_suffix - Tᴏ Sᴇᴇ Yᴏᴜʀ Sᴜғғɪx !!
+➢ /del_suffix - Tᴏ Dᴇʟᴇᴛᴇ Yᴏᴜʀ Sᴜғғɪx !!
+
+Exᴀᴍᴩʟᴇ:- `/set_suffix @OttSandhu`
+Exᴀᴍᴩʟᴇ:- `/set_prefix @OttSandhu`
+"""
+    
+    DEV_TXT = """<b><u>Sᴩᴇᴄɪᴀʟ Tʜᴀɴᴋꜱ & Dᴇᴠᴇʟᴏᴩᴇʀꜱ</b></u>
+    
+» 𝗦𝗢𝗨𝗥𝗖𝗘 𝗖𝗢𝗗𝗘 : <a href=https://t.me/Baii_Ji>Contact Owner</a>"""
+
+    SEND_METADATA = """
+❪ SET CUSTOM METADATA ❫
+
+☞ Fᴏʀ Exᴀᴍᴘʟᴇ:-
+
+`--change-title TG - OttSandhu
+--change-video-title [OttSandhu]
+--change-audio-title [OttSandhu]
+--change-subtitle-title [OttSandhu]
+--change-author [OttSandhu]`
+
+📥 Fᴏʀ Hᴇʟᴘ Cᴏɴᴛ. @Baii_Ji
+"""
+    
+    RKN_PROGRESS = """<b>\n
+╭━━━━━━❰PROCESSING...❱━━━➣
+┣⪼ 🗃️ ꜱɪᴢᴇ: {1} | {2}
+┣⪼ ⏳️ ᴅᴏɴᴇ : {0}%
+┣⪼ 🚀 ꜱᴩᴇᴇᴅ: {3}/s
+┣⪼ ⏰️ ᴇᴛᴀ: {4}
+╰━━━━━━━━━━━━━━━➣ </b>"""
